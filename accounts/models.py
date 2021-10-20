489281d9243c0179111dd268b8b535ecf3eac086
Luis Carlos <lu.ca.oliveira41@gmail.com>
2021-10-20 17:31:30 -0400

    refactor: removendo métodos não utilizados e configurações defasadas

--- accounts/models.py
+++ accounts/models.py
@@ -8,39 +8,44 @@ from django.utils.translation import ugettext_lazy as _
 
 from accounts.managers import UserManager
 
+
 class User(AbstractBaseUser, PermissionsMixin):
-    first_name = models.CharField(_('first name'), max_length=30)
-    last_name = models.CharField(_('last name'), max_length=30)
-    email = models.EmailField(_('email address'), max_length=255, unique=True)
-    is_staff = models.BooleanField(_('staff status'), default=False, help_text=_('Designates whether the user can log into this admin site.'))
-    is_active = models.BooleanField(_('active'), default=True, help_text=_('Designates whether this user should be treated as active. Unselect this instead of deleting accounts.'))
-    is_trusty = models.BooleanField(_('trusty'), default=False, help_text=_('Designates whether this user has confirmed his account.'))
-    date_joined = models.DateTimeField(_('date joined'), default=timezone.now)
-    
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    email = models.EmailField(max_length=255, unique=True)
+    is_staff = models.BooleanField(
+        default=False, help_text='Designates whether the user can log into this admin site.')
+    is_active = models.BooleanField(
+        default=True, help_text='Designates whether this user should be treated as active. Unselect this instead of deleting accounts.')
+    is_trusty = models.BooleanField(
+        default=False, help_text='Designates whether this user has confirmed his account.')
+    date_joined = models.DateTimeField(default=timezone.now)
+
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = ['first_name', 'last_name']
     objects = UserManager()
+
     class Meta:
-        verbose_name = _('user')
-        verbose_name_plural = _('users')
-    
+        verbose_name = 'user'
+        verbose_name_plural = 'users'
+
     def get_full_name(self):
         full_name = f'${self.first_name} ${self.last_name}'
         return full_name.strip()
-    
+
     def get_short_name(self):
         return self.first_name
 
     def email_user(self, subject, message, from_email=None):
         send_mail(subject, message, from_email, [self.email])
 
-class UserAddress(Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='address')
-    street = models.CharField(_('street'), max_length=30)
-    number = models.CharField(_('number'), max_length=10)
-    complement = models.CharField(_('complement'), max_length=50, blank=True)
-    district =  models.CharField(_('district'), max_length=50)
-    city =  models.CharField(_('city'), max_length=30)
-    state =  models.CharField(_('state'), max_length=20)
 
-    
+class UserAddress(Model):
+    user = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='address')
+    street = models.CharField(max_length=30)
+    number = models.CharField(max_length=10)
+    complement = models.CharField(max_length=50, blank=True)
+    district = models.CharField(max_length=50)
+    city = models.CharField(max_length=30)
+    state = models.CharField(max_length=20)
--- accounts/usecases.py
+++ accounts/usecases.py
@@ -3,21 +3,12 @@ from accounts.models import User, UserAddress
 from accounts.types import NewUser, UserLogin
 
 
-def create_user_account(user_payload:NewUser) -> User:
+def create_user_account(user_payload: NewUser) -> User:
     addresses = user_payload.pop("address")
     password = user_payload.pop("password")
-    user_account:User = User.objects.create(**user_payload)
+    user_account: User = User.objects.create(**user_payload)
     user_account.set_password(password)
     for addr in addresses:
         UserAddress.objects.create(user=user_account, **addr)
     user_account.save()
     return user_account
-
-def user_login(email, password)->Union[User, None]:
-    user:User = User.objects.get(email=email)
-    if user:
-        password_valid = user.check_password(password)
-        if password_valid:
-            return user
-    return None
-
